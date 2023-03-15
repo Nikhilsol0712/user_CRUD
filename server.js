05ee9b7ed865ec8d5c6e97bcefe011b6b027f725
Nikhilsol0712 <solankiniks0712@gmail.com>
2023-03-15 12:34:07 +0530

    cors install

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const { sequelize } = require("./models/index");
 //adding body-parser middleware , it helps the server to reads the data.
 const bodyParser = require("body-parser");
+const cors = require("cors");
 require("dotenv").config();
 const userRoutes = require("./routes/user.routes");
-const app = express();
 
+const app = express();
+app.use(cors());
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", async (req, res) => res.send("<h1>hellow<h1>"));
 userRoutes(app);
