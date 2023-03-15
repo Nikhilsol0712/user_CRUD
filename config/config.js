45d248f961716bf79a342e3fe615be5b0d8f51fa
Nikhilsol0712 <solankiniks0712@gmail.com>
2023-03-15 12:29:13 +0530

    confi.json update

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 module.exports = {
   development: {
-    username: "root",
-    password: "Fbking6nikhil",
-    database: "User",
-    host: "127.0.0.1",
+    username: process.env.MYSQL_ADDON_USER,
+    password: process.env.MYSQL_ADDON_PASSWORD,
+    database: process.env.MYSQL_ADDON_DB,
+    host: process.env.MYSQL_ADDON_HOST,
     dialect: "mysql",
   },
   test: {
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
-
+const { sequelize } = require("./models/index");
 //adding body-parser middleware , it helps the server to reads the data.
 const bodyParser = require("body-parser");
-
+require("dotenv").config();
 const userRoutes = require("./routes/user.routes");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.get("/", async (req, res) => res.send("<h1>hellow<h1>"));
 userRoutes(app);
-
-const PORT = 3000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, async () => {
+  await sequelize.sync();
   console.log("server is listning to PORT:", PORT);
 });
